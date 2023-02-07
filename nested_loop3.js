00df07a85d5e5b725804ad2fa7c44a839fbe9d81
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-07 10:07:37 +0530

    prob on prime no using nested loop

---
+++ nested_loop3.js
@@ -0,0 +1,15 @@
+//prob on prime no
+
+for(let i=1;i<=10;i++){
+let number= i;
+let count=0;
+for(let j=1; j<=number;j++){
+  if(number%j==0){
+    count++;
+  }
+}
+if(count==2){
+  console.log(i);
+}
+
+}
